8060df75b08190afccca82b1d014c9a73d0e3e07
Gautier Bureau <gautier.bureau@gmail.com>
2021-11-03 12:51:52 +0100

    add partition.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using std::cout;
 using std::domain_error;
 
 using Size = unsigned;
+using Capacity = Size;
 using Name = string;
 
 inline Size operator"" _bytes (unsigned long long value) {
@@ -40,7 +41,7 @@ class Element : private NonCopyable {
   const Folder* const parent_ = nullptr;
 };
 
-class File : public Element {
+class File final : public Element {
  public:
   friend class Folder;
   virtual Size getSize() const override;
@@ -52,21 +53,34 @@ class File : public Element {
 
 class Folder : public Element {
  public:
-  static Folder& getRoot();
+  // static Folder& getRoot();
   File& createFile(const Name& fileName, Size fileSize);
   Folder& createFolder(const Name& folderName);
   virtual Size getSize() const override;
   void removeElement(const Name& elementName);
+ protected:
+  virtual ~Folder();
+  using Element::Element;
  private:
   using Key = Name;
 //  explicit Folder(const Name& folderName);
-  using Element::Element;
-  virtual ~Folder();
   static Key keyFromName(const Name& name);
 
   std::map<Key, const Element*> children_;
 
   const Key checkNameAvailability(const Name& elementName) const;
+};
+
+class Partition final : public Folder {
+ public:
+  static Partition& getInstance();
+  using Folder::getSize;
+  friend File;
+ private:
+  Partition(const Name& name, Capacity capacity);
+  ~Partition() = default;
+  void checkRemainingSize(Size desiredSize) const;
+  Size capacity_;
 };
 
 Element::Element(const Name& name, const Folder* parent) : name_{name}, parent_{parent} {
@@ -79,10 +93,10 @@ const Name Element::getAbsoluteName() const {
   return leftPart + name_;
 }
 
-Folder& Folder::getRoot() {
-  static Folder root{"/", nullptr};
-  return root;
-}
+//Folder& Folder::getRoot() {
+//  static Folder root{"/", nullptr};
+//  return root;
+//}
 
 //Folder::Folder(const Name& name) : Element { name } {
 //}
@@ -130,6 +144,7 @@ Size Folder::getSize() const {
 }
 
 File::File(const Name& fileName, Size size, const Folder& parent) : Element { fileName, &parent}, size_ { size } {
+  Partition::getInstance().checkRemainingSize(size);
 }
 
 Size File::getSize() const {
@@ -142,15 +157,30 @@ Folder::Key Folder::keyFromName(const Name& name) {
   return key;
 }
 
+Partition::Partition(const Name &name, Capacity capacity) : Folder{name, nullptr}, capacity_(capacity) {
+}
+
+Partition& Partition::getInstance() {
+  static Partition instance{"/", 10'000_bytes};
+  return instance;
+}
+
+void Partition::checkRemainingSize(Size desiredSize) const {
+  if (capacity_ - getSize() < desiredSize)
+    throw domain_error {"capacity overflow."};
+}
+
 int main() {
   try {
-    Folder& r1{ Folder::getRoot() };
+    // Folder& r1{ Folder::getRoot() };
+    Partition& r1{ Partition::getInstance() };
     Folder& r2{ r1.createFolder("r2") };
     // Folder& r2_bis{ r1.createFolder("R2") };
     File& f1 = r1.createFile("f1", 899_bytes);
     // File& f1_bis = r1.createFile("F1", 899_bytes);
     // File& f1_empty = r1.createFile("", 899_bytes);
     File& f2{ r2.createFile("f2", 1234_bytes) };
+    // File& f3{ r2.createFile("f3", 12'340_bytes) };
     cout << r1.getSize() << " bytes\n";
     //r2.removeElement("f2");
 
